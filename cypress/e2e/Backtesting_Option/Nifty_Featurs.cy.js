6c2d686e40bb79243e7ce38baa510d59aa5ca4b6
Tester1 <137996855+tester-24@users.noreply.github.com>
2024-11-04 14:46:06 +0530

    Strike_Reports.

--- cypress/e2e/Backtesting_Option/Nifty_Featurs.cy.js
+++ cypress/e2e/Backtesting_Option/Nifty_Featurs.cy.js
@@ -142,12 +142,13 @@ it('Nifty Feature', () => {
     // verify leg 1 is created or not
     cy.get('.font_med > .col-12 > .blue_ucase > :nth-child(1)').should("be.visible");
 
-    ////// Target profit
-    cy.get('#LegBuilder1 > .leg_inner_form_wrap > .is_new_leg > :nth-child(1) > .form_item_wrap > .text-gray-800 > .ml-2').click();
-
+  ////// Target profit
+   // cy.get('#LegBuilder1 > .leg_inner_form_wrap > .is_new_leg > :nth-child(1) > .form_item_wrap > .text-gray-800 > .ml-2').click();
+   cy.get('.padding_x20_y10 > .leg_inner_form_wrap > .is_new_leg > :nth-child(1) > .form_item_wrap > .text-gray-800 > .ml-2').click();
     // select dropdown
     cy.xpath("(//span[@class='k-input-value-text'][normalize-space()='Points'])[1]").click();
-    cy.contains('Underlying Points').click();
+   //cy.contains('Underlying Points').click();
+   cy.xpath('/html/body/app-root/app-layout/app-option-structure/div[2]/div/div/div/div/div[2]/div/div/div/form[2]/div[5]/div/div/div/div/div[4]/div[1]/div/div/div/div[1]/div/kendo-dropdownlist/span/span').click()
 
     // Enter Target
     cy.get('.is_new_leg > :nth-child(1) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .form-control').clear();
@@ -166,8 +167,8 @@ it('Nifty Feature', () => {
     // cy.get('#LegBuilder1 > .leg_inner_form_wrap > .is_new_leg > :nth-child(1) > .form_item_wrap > .text-gray-800 > .ml-2').click();
 
     ////// Stop Loss
-    cy.get('#LegBuilder1 > .leg_inner_form_wrap > .is_new_leg > :nth-child(2) > .form_item_wrap > .text-gray-800 > .ml-2').click();
-
+    //cy.get('#LegBuilder1 > .leg_inner_form_wrap > .is_new_leg > :nth-child(2) > .form_item_wrap > .text-gray-800 > .ml-2').click();
+    cy.get('.padding_x20_y10 > .leg_inner_form_wrap > .is_new_leg > :nth-child(2) > .form_item_wrap > .text-gray-800 > .ml-2').click()
     cy.get('.is_new_leg > :nth-child(2) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .form-control').clear();
     cy.get('.is_new_leg > :nth-child(2) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .form-control').should("be.visible");
 
@@ -181,8 +182,8 @@ it('Nifty Feature', () => {
     cy.get('.is_new_leg > :nth-child(2) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .form-control').type('5');
 
     /////// Trail SL
-    cy.get('#LegBuilder1 > .leg_inner_form_wrap > .is_new_leg > :nth-child(3) > .form_item_wrap > .text-gray-800 > .ml-2').click();
-
+   // cy.get('#LegBuilder1 > .leg_inner_form_wrap > .is_new_leg > :nth-child(3) > .form_item_wrap > .text-gray-800 > .ml-2').click();
+   cy.get('.padding_x20_y10 > .leg_inner_form_wrap > .is_new_leg > :nth-child(3) > .form_item_wrap > .text-gray-800 > .ml-2').click()
     /// X value
     cy.get('.py-0 > .form-control').type('0');
     cy.get('#LegInstrumentMovesValError_0 > .text-danger').should("be.visible");
@@ -216,8 +217,11 @@ it('Nifty Feature', () => {
 
     ////// Re-Entry on Target
     cy.get('.full_wrap.d-flex > .full_wrap > .text-gray-800 > .ml-2').click();
-    cy.get('#LegBuilder1 > .leg_inner_form_wrap > :nth-child(5) > :nth-child(1) > .form_item_wrap > :nth-child(2) > .d-flex > .full_wrap').click().type('{downarrow}{downarrow}{Enter}')
-
+   //cy.get('#LegBuilder1 > .leg_inner_form_wrap > :nth-child(5) > :nth-child(1) > .form_item_wrap > :nth-child(2) > .d-flex > .full_wrap').click().type('{downarrow}{downarrow}{Enter}')
+   cy.xpath('/html/body/app-root/app-layout/app-option-structure/div[2]/div/div/div/div/div[2]/div/div/div/form[2]/div[5]/div/div/div/div/div[5]/div[1]/div/div[2]/div/div[1]/div/kendo-dropdownlist/button').click({force:true})
+   //slect values from dropdown
+   cy.wait(1000)
+   cy.xpath('/html[1]/body[1]/app-root[1]/kendo-popup[1]/div[1]/kendo-list[1]/div[1]/ul[1]/li[3]/span[1]').click()
     cy.get(':nth-child(5) > :nth-child(1) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .form-control').clear();
     cy.get(':nth-child(1) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .ng-star-inserted').should("be.visible");
 
@@ -237,8 +241,8 @@ it('Nifty Feature', () => {
 
 
     ////// Re-Entry on SL
-    cy.get('#LegBuilder1 > .leg_inner_form_wrap > :nth-child(5) > :nth-child(2) > .form_item_wrap > .text-gray-800 > .ml-2').click();
-
+   // cy.get('#LegBuilder1 > .leg_inner_form_wrap > :nth-child(5) > :nth-child(2) > .form_item_wrap > .text-gray-800 > .ml-2').click();
+   cy.get('.padding_x20_y10 > .leg_inner_form_wrap > :nth-child(5) > :nth-child(2) > .form_item_wrap > .text-gray-800 > .ml-2').click()
     cy.get(':nth-child(5) > :nth-child(2) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .form-control').clear();
     cy.get(':nth-child(5) > :nth-child(2) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .ng-star-inserted').should("be.visible");
 
@@ -257,8 +261,8 @@ it('Nifty Feature', () => {
     cy.get(':nth-child(5) > :nth-child(2) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .form-control').type('5');
 
     ////// Simple Momentum
-    cy.get('#LegBuilder1 > .leg_inner_form_wrap > :nth-child(5) > :nth-child(3) > .form_item_wrap > .text-gray-800 > .ml-2').click();
-
+    //cy.get('#LegBuilder1 > .leg_inner_form_wrap > :nth-child(5) > :nth-child(3) > .form_item_wrap > .text-gray-800 > .ml-2').click();
+    cy.get('.padding_x20_y10 > .leg_inner_form_wrap > :nth-child(5) > :nth-child(3) > .form_item_wrap > .text-gray-800 > .ml-2').click()
     cy.get(':nth-child(5) > :nth-child(3) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .form-control').clear();
     cy.get(':nth-child(3) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .text-danger').should("be.visible");
 
@@ -273,6 +277,7 @@ it('Nifty Feature', () => {
     cy.get(':nth-child(3) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .text-danger').should("be.visible");
 
     cy.get(':nth-child(5) > :nth-child(3) > .form_item_wrap > :nth-child(2) > .d-flex > .border-0 > .form-control').type('5');
+
 
 
     ////// Range Break out
@@ -328,80 +333,15 @@ it('Nifty Feature', () => {
 
     cy.get('.form-control.mr-2 > .form-control').type('2');
     cy.get(':nth-child(3) > .col-12 > .form_item_wrap > :nth-child(2) > .d-flex > :nth-child(3) > .form-control').type('4');
-
+        cy.get("#datepicker-4").clear();
+    cy.get("div.ng-star-inserted > .text-danger").should("be.visible");
+  
+    cy.get("#datepicker-4").type("2024-2024");
+ 
     //// Click on Run Backtesting
     cy.get('.secondary').click();
 
     cy.get('.loader_numb').should("be.visible");
 
-    // 3) Type
-
-    // 4) Entry Time
-    cy.wait(85000);
-   // cy.xpath("(//kendo-icon[@class='k-i-clock k-button-icon k-icon ng-star-inserted'])[2]").click();
-
-
-    cy.xpath("(//input[@id='timepicker-1'])[1]").click({force:true});
-    cy.xpath("(//input[@id='timepicker-1'])[1]").type("09.10");
-    cy.scrollTo(1000, 0);
-    cy.xpath("//a[normalize-space()='Save']").click({ force: true });
-    cy.scrollTo(1000, 0);
-
-    cy.xpath("(//input[@id='timepicker-1'])[1]").click({force:true});
-    cy.xpath("(//input[@id='timepicker-1'])[1]").type("09.05");
-    
-    cy.scrollTo(1000, 0);
-    cy.xpath("//a[normalize-space()='Save']").click({ force: true });
-    cy.scrollTo(1000, 0);
-
-    cy.wait(2000);
-    cy.xpath("(//input[@id='timepicker-1'])[1]").click();
-    cy.xpath("(//input[@id='timepicker-1'])[1]").type("09:20");
-    cy.scrollTo(1000, 0);
-    cy.xpath("//a[normalize-space()='Save']").click({ force: true });
-    cy.scrollTo(1000, 0);
-
-    cy.wait(2000);
-    cy.xpath("(//input[@id='timepicker-1'])[1]").click({force:true});
-    cy.xpath("(//input[@id='timepicker-1'])[1]").type("08:20");
-    cy.scrollTo(1000, 0);
-    cy.xpath("//a[normalize-space()='Save']").click({ force: true });
-    cy.scrollTo(1000, 0);
-
-    cy.wait(2000);
-    cy.xpath("(//input[@id='timepicker-1'])[1]").click({force:true});
-    cy.xpath("(//input[@id='timepicker-1'])[1]").type("09:20");
-    cy.scrollTo(1000, 0);
-    cy.xpath("//a[normalize-space()='Save']").click({ force: true });
-    cy.scrollTo(1000, 0);
-
-    // 5) Exit Time
-
-    cy.xpath("(//input[@id='timepicker-2'])[1]").click({force:true});
-    cy.xpath("(//input[@id='timepicker-2'])[1]").type("09:10");
-    cy.scrollTo(1000, 0);
-    cy.xpath("//a[normalize-space()='Save']").click({ force: true });
-    cy.scrollTo(1000, 0);
-
-    cy.wait(2000);
-    cy.xpath("(//input[@id='timepicker-2'])[1]").click({force:true});
-    cy.xpath("(//input[@id='timepicker-2'])[1]").type("09:20");
-    cy.scrollTo(1000, 0);
-    cy.xpath("//a[normalize-space()='Save']").click({ force: true });
-    cy.scrollTo(1000, 0);
-
-    cy.wait(2000);
-    cy.xpath("(//input[@id='timepicker-2'])[1]").click({force:true});
-    cy.xpath("(//input[@id='timepicker-2'])[1]").type("08:20");
-    cy.scrollTo(1000, 0);
-    cy.xpath("//a[normalize-space()='Save']").click({ force: true });
-    cy.scrollTo(1000, 0);
-
-    cy.wait(2000);
-    cy.xpath("(//input[@id='timepicker-2'])[1]").click({force:true});
-    cy.xpath("(//input[@id='timepicker-2'])[1]").type("15:15");
-    cy.scrollTo(1000, 0);
-    cy.xpath("//a[normalize-space()='Save']").click({ force: true });
-    cy.scrollTo(1000, 0);
-
+  
 })
