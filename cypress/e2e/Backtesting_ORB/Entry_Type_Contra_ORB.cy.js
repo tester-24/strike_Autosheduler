ee4df10564dd9e3c8888c15f37ea46e7452fbc2d
Tester1 <137996855+tester-24@users.noreply.github.com>
2024-11-13 11:58:21 +0530

    Strike_Reports.

--- cypress/e2e/Backtesting_ORB/Entry_Type_Contra_ORB.cy.js
+++ cypress/e2e/Backtesting_ORB/Entry_Type_Contra_ORB.cy.js
@@ -44,7 +44,8 @@ it("Option Backtesting", () => {
      //Click on  Symbol dropdown
      cy.wait(1000)
     // cy.get(':nth-child(1) > :nth-child(1) > .form_item_wrap > .full_wrap > .form-control').click({force:true})
-    cy.get(':nth-child(1) > .leg_inner_form_wrap > :nth-child(1) > :nth-child(3)').click({force:true})
+  //  cy.get(':nth-child(1) > .leg_inner_form_wrap > :nth-child(1) > :nth-child(3)').click({force:true})
+     cy.xpath('/html/body/app-root/app-layout/app-orb-structure/div/div/div/div/div/div/div[2]/div/div/form[2]/div[1]/div/div[1]/div[1]/div/div/kendo-multiselect/kendo-taglist/kendo-searchbar/input').click()
      //select symbol 3MINDIA
      cy.wait(500)
      cy.xpath('/html[1]/body[1]/app-root[1]/kendo-popup[1]/div[1]/kendo-list[1]/div[1]/ul[1]/li[3]').click()
