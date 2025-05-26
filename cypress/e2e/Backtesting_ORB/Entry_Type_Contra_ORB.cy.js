89db86132041e177c9cdf65bb2df83eee9149566
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-05-26 09:46:44 +0530

    Strike_Reports.

--- cypress/e2e/Backtesting_ORB/Entry_Type_Contra_ORB.cy.js
+++ cypress/e2e/Backtesting_ORB/Entry_Type_Contra_ORB.cy.js
@@ -143,7 +143,7 @@ it("Option Backtesting", () => {
         //click on runbacktest
         cy.get('.secondary').click()
         //validation for data 
-        cy.wait(10000)
+        cy.wait(60000)
         cy.get('#BackTestResultView > :nth-child(2)').should('be.visible')
  
    
